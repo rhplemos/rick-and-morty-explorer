8313b47bfd701c5bbbca351c37efc5f75d0ebbc0
Rafael <rhplemos@gmail.com>
2023-06-28 19:12:44 -0300

    feat: first models

---
+++ lib/models/character_model.dart
@@ -0,0 +1,31 @@
+import 'location_model.dart';
+
+class CharacterModel {
+  int id;
+  String name;
+  String status;
+  String species;
+  String type;
+  String gender;
+  Location origin;
+  Location location;
+  String image;
+  List<String> episode;
+  String url;
+  String created;
+
+  CharacterModel({
+    required this.id,
+    required this.name,
+    required this.status,
+    required this.species,
+    required this.type,
+    required this.gender,
+    required this.origin,
+    required this.location,
+    required this.image,
+    required this.episode,
+    required this.url,
+    required this.created,
+  });
+}
---
+++ lib/models/location_model.dart
@@ -0,0 +1,9 @@
+class Location {
+  String name;
+  String url;
+
+  Location({
+    required this.name,
+    required this.url,
+  });
+}
