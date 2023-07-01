7ec582c100f20382aa5cfbcee45d6f3f5cb6b4dc
Rafael <rhplemos@gmail.com>
2023-07-01 09:24:25 -0300

    feat: location model

--- lib/models/location_model.dart
+++ lib/models/location_model.dart
@@ -1,9 +1,43 @@
-class Location {
+class LocationModel {
+  int id;
   String name;
+  String type;
+  String dimension;
+  List<String> residents;
   String url;
+  String created;
 
-  Location({
+  LocationModel({
+    required this.id,
     required this.name,
+    required this.type,
+    required this.dimension,
+    required this.residents,
     required this.url,
+    required this.created,
   });
+
+  factory LocationModel.fromMap(Map<String, dynamic> map) {
+    return LocationModel(
+      id: map['id'] as int,
+      name: map['name'] as String,
+      type: map['type'] as String,
+      dimension: map['dimension'] as String,
+      residents: List<String>.from(map['residents'] as List),
+      url: map['url'] as String,
+      created: map['created'] as String,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': this.id,
+      'name': this.name,
+      'type': this.type,
+      'dimension': this.dimension,
+      'residents': this.residents,
+      'url': this.url,
+      'created': this.created,
+    };
+  }
 }
